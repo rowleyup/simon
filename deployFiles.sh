92195c4c14fe32381bbe8efe027c0b03b6a79b9d
Eric Upton-Rowley <rowleyup@gmail.com>
2023-02-07 23:20:53 -0700

    edited deploy file

--- deployFiles.sh
+++ deployFiles.sh
@@ -2,17 +2,17 @@
 
 while getopts k:h:s: flag
 do
-	case "${flag}" in
-		k) key=${OPTARG};;
-		h) hostname=${OPTARG};;
-		s) services=${OPTARG};;
-	esac
+    case "${flag}" in
+        k) key=${OPTARG};;
+        h) hostname=${OPTARG};;
+        s) service=${OPTARG};;
+    esac
 done
 
 if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
-	printf "\nMissing required parameter.\n"
-	printf " syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
-	exit 1
+    printf "\nMissing required parameter.\n"
+    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    exit 1
 fi
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
